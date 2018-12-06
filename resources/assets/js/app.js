d7e5e70a02a9972ebcc997358e723d5c7e210d50
Mario Patronelli <mario@raffles.com.ar>
2018-12-06 23:00:25 +0000

    Fix eslint shown errors and delete ExampleComponent.vue

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -5,9 +5,20 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+require("./bootstrap")
 
-window.Vue = require('vue');
+import Vue from "vue"
+
+/**
+ * The following block of code may be used to automatically register your
+ * Vue components. It will recursively scan this directory for the Vue
+ * components and automatically register them with their "basename".
+ *
+ * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+ */
+
+const files = require.context("./", true, /\.vue$/i)
+files.keys().map(key => Vue.component(key.split("/").pop().split(".")[0], files(key)))
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -15,8 +26,8 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
-
 const app = new Vue({
-    el: '#app'
-});
+    el: "#app"
+})
+
+export default app
