ee2db2c066eecafc3aa351e6e2ef115578d1fa35
Mario Patronelli <mario@raffles.com.ar>
2018-12-07 02:27:01 +0000

    Install and configure caffeinated/shinobi

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,14 @@
 
 namespace Raffles\Models;
 
+use Caffeinated\Shinobi\Traits\ShinobiTrait;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, Notifiable;
+    use HasApiTokens, Notifiable, ShinobiTrait;
 
     /**
      * The attributes that are mass assignable.
