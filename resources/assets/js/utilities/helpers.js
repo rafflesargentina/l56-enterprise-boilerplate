b742935ae99a4ce930ea6803ca612b909e7215b1
Mario Patronelli <mario@raffles.com.ar>
2019-01-04 14:36:52 +0000

    Add slugify helper and directive

--- resources/assets/js/utilities/helpers.js
+++ resources/assets/js/utilities/helpers.js
@@ -67,6 +67,34 @@ export function saveState(key, state, remember) {
     }
 }
 
+export function slugify(text) {
+    var slug = ""
+    // Change to lower case
+    var lower = text.toLowerCase()
+    // Letter "e"
+    slug = lower.replace(/e|é|è|ẽ|ẻ|ẹ|ê|ế|ề|ễ|ể|ệ/gi, "e")
+    // Letter "a"
+    slug = slug.replace(/a|á|à|ã|ả|ạ|ă|ắ|ằ|ẵ|ẳ|ặ|â|ấ|ầ|ẫ|ẩ|ậ/gi, "a")
+    // Letter "o"
+    slug = slug.replace(/o|ó|ò|õ|ỏ|ọ|ô|ố|ồ|ỗ|ổ|ộ|ơ|ớ|ờ|ỡ|ở|ợ/gi, "o")
+    // Letter "u"
+    slug = slug.replace(/u|ú|ù|ũ|ủ|ụ|ư|ứ|ừ|ữ|ử|ự/gi, "u")
+    // Trim the last whitespace
+    slug = slug.replace(/\s*$/g, "")
+    // Change whitespace to "-"
+    slug = slug.replace(/\s+/g, "-")
+    // Replace multiple - with single -
+    slug = slug.replace(/--+/g, "-")
+    // Replace @ with "at"
+    slug = slug.replace(/@/g, "-at-")
+    // Replace & with "and"
+    slug = slug.replace(/&/g, "-and-")
+    // Remove all non-word chars
+    slug = slug.replace(/[^\w-]+/g, "")
+
+    return slug
+}
+
 export function strLimit(value = "", limit = 30, end = "...") {
     return null !== value && value.length > limit ? value.substr(0, (limit - value.length)) + end : value
 }
